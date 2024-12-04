1d03146ba75d449772939b8104ed5bab4acf78f7
iwillsettyou <bmate20050911@gmail.com>
2024-12-04 08:59:28 +0100

    Done with tests

--- Program.cs
+++ Program.cs
@@ -7,4 +7,31 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    public class Car(int carId, string model, int year, string maufacturer)
+    {
+        public int CarId => carId;
+        public string Model => model;
+        public int Year => year;
+        public string Manufacturer => maufacturer;
+
+        // 1. Metódus: Visszaadja a gépjármű teljes adatát formázott stringben
+        public string GetCarInfo()
+        {
+            return $"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, CarId: {CarId}";
+        }
+
+        // 2. Metódus: Ellenőrzi, hogy a gépjármű gyártási éve a jelenlegi évnél újabb-e
+        public bool IsNewCar()
+        {
+            var currentYear = DateTime.Now.Year;
+            return Year >= currentYear;
+        }
+
+        // 3. Metódus: Visszaadja a gépjármű életkorát (aktuális év - gyártási év)
+        public int GetCarAge()
+        {
+            var currentYear = DateTime.Now.Year;
+            return currentYear - Year;
+        }
+    }
 }
